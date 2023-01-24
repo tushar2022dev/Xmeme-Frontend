d828464a10212b43e854433be6813a93c7b6ae05
Tushar <megamind.25.08@gmail.com>
2023-01-25 01:53:24 +0530

    v2

--- feed.js
+++ feed.js
@@ -51,7 +51,7 @@ function onPageLoad(){
              s.toLocaleString()
             const time = s.getDay()+'/'+s.getMonth()+1+'  '+s.getHours()+':'+s.getMinutes()
           
-            result =result+ '<div class=" my-16 bg-white rounded-lg mx-16 mr-16 pt-6 shadow-2xl outline-1 outline-double outline-gray-200"><div class="flex ml-4 "><div><img src="./Resources/avatar.png" class="rounded-t-full w-9 h-9"></div><h2 class="ml-3 text-base font-semibold mt-3">'+memeFile[i]['username']+'</h2></div><p class=" mb-3 mt-6 text-gray-700 ml-2 text-xs">'+memeFile[i]['caption']+'</p><img src="'+memeFile[i]['image']+'" class="h-[30rem] w-full bg-cover bg-no-repeat "><div class="flex flex-row"> <p class="ml-3 text-center pt-4 text-sm text-gray-600 font-semibold" id="date">'+ s+'</p> </div></div>';
+            result =result+ '<div class="max-[700px]:mx-8 my-16 bg-white rounded-lg mx-16 mr-16 pt-6 shadow-2xl outline-1 outline-double outline-gray-200"><div class="flex ml-4 "><div><img src="./Resources/avatar.png" class="rounded-t-full w-9 h-9"></div><h2 class="ml-3 text-base font-semibold mt-3">'+memeFile[i]['username']+'</h2></div><p class=" mb-3 mt-6 text-gray-700 ml-2 text-xs">'+memeFile[i]['caption']+'</p><img src="'+memeFile[i]['image']+'" class="max-[400px]:h-[24rem] h-[30rem] w-full bg-cover bg-no-repeat "><div class="flex flex-row"> <p class="ml-3 text-center pt-4 text-sm text-gray-600 font-semibold" id="date">'+ s+'</p> </div></div>';
            
           }
                 document.getElementById("meme_space").innerHTML= result 
